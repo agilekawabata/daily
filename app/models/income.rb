051b5928426c616281db38cf31e769853b9174ac
Sho DOUHASHI <douhashi@gmail.com>
2012-09-29 03:28:38 +0900

    収入

--- app/models/income.rb
+++ app/models/income.rb
@@ -8,4 +8,15 @@ class Income < ActiveRecord::Base
   DATE_UNIT_MONTH = "month"
   DATE_UNIT_DAY = "day"
   DATE_UNIT_SELECTION = [["年", DATE_UNIT_YEAR], ["月", DATE_UNIT_MONTH], ["日", DATE_UNIT_DAY]]
+  
+  def calc_daily
+    case date_unit
+    when DATE_UNIT_DAY
+      amount * quantity
+    when DATE_UNIT_MONTH
+      amount * quantity * 12 / 365
+    when DATE_UNIT_YEAR
+      amount * quantity / 365
+    end
+  end
 end
--- spec/models/income_spec.rb
+++ spec/models/income_spec.rb
@@ -1,5 +1,51 @@
+# coding: utf-8
 require 'spec_helper'
 
 describe Income do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#calc_daily" do
+    context "date_unitが日単位の場合" do
+      before(:each) do
+        @uriage = Income.create(
+          :title => "チョコレートの売上", 
+          :date_unit => Income::DATE_UNIT_DAY, 
+          :quantity => 10, 
+          :amount => 100)
+      end
+      it "は、１日あたりの金額を返すべき" do
+        @uriage.calc_daily.should == 1000
+      end
+    end
+    
+    context "date_unitが月単位の場合" do
+      before(:each) do
+        @income = Income.create(
+          :title => "給料", 
+          :date_unit => Income::DATE_UNIT_MONTH, 
+          :quantity => 1, 
+          :amount => 300000)
+        @part = Income.create(
+          :title => "給料(嫁さんのパート)", 
+          :date_unit => Income::DATE_UNIT_MONTH, 
+          :quantity => 1, 
+          :amount => 60000)
+      end
+      it "は、１日あたりの金額を返すべき" do
+        @income.calc_daily.should == 9863
+        @part.calc_daily.should == 1972
+      end
+    end
+    
+    context "date_unitが年単位の場合" do
+      before(:each) do
+        @otoshidama = Income.create(
+          :title => "お年玉", 
+          :date_unit => Income::DATE_UNIT_YEAR, 
+          :quantity => 3, 
+          :amount => 10000)
+      end
+      it "は、１日あたりの金額を返すべき" do
+        @otoshidama.calc_daily.should == 82
+      end
+    end
+  end
 end
