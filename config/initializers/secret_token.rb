a53ac335b15134afd6c27bde8453247ed1786da8
Kawabata Mitsuyoshi <kawabata.agile@gmail.com>
2012-09-29 00:02:35 +0900

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Daily::Application.config.secret_token = 'dad5b52400ea95acb5e3e77393f12ebf4a45c7a57774c7045780c5c7c7ed59276e56de37f18af4c909fee411698707977f90cd3ca51c14d73a97ece51b200618'
