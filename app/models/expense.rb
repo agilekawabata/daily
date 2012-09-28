4e79ac7d175d753a86e6ad8d02a26d67ed25505a
Sho DOUHASHI <douhashi@gmail.com>
2012-09-29 01:25:10 +0900

    remove view spec

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,6 @@
+# coding: utf-8
 class Expense < ActiveRecord::Base
   attr_accessible :amount, :date_unit, :quantity, :title
+  
+  DATE_UNIT_SELECTION = [["年", "year"], ["月", "month"], ["日", "day"]]
 end
